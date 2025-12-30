08e90f5a3a66723189fe4fdf9e7887c2f7de10a2
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-30 22:56:32 +0500

    Added toggleFlag

---
+++ src/features/toggleFlag.ts
@@ -0,0 +1,18 @@
+import { toggleFeatureFlag } from "../entities/featureFlag/model/action";
+import { updateFeatureFlagApi } from "../api/updateFeatureFlag.api";
+import type { Environment, FeatureFlag } from "../entities/featureFlag/model/types";
+
+export async function toggleFlag(
+    flag: FeatureFlag,
+    environment: Environment
+) : Promise<FeatureFlag> {
+    const updated = toggleFeatureFlag(flag, environment);
+
+    await updateFeatureFlagApi(
+            flag.id,
+            environment,
+            updated.environments[environment]
+    );
+
+    return updated;
+};
