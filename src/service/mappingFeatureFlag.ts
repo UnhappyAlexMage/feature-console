094610ff34d7a5e1bf45464eef15816891ba6604
UnhappyAlex <alexandr.bro03@gmail.com>
2026-01-06 22:51:00 +0500

    Changed configuration

--- src/service/mappingFeatureFlag.ts
+++ src/service/mappingFeatureFlag.ts
@@ -1,7 +1,8 @@
-//Маппинг — это процесс создания нового чистого объекта из «сырого» JSON.
-//
+//Маппинг — это процесс создания нового чистого объекта из «сырого» JSON
+// функции, которые принимают валидный DTO и превращают его в удобный для фронтенда объект.
+
 import type { FeatureFlag } from "../entities/featureFlag/model/types";
-import type { FeatureFlagDto } from "./types";
+import type { FeatureFlagDto } from "../api/types";
 
 export function mapFeatureFlagDtoDomain(
     dto: FeatureFlagDto
