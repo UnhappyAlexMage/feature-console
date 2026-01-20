2398e3b7524c594a321d760a55cba487cdecc5d4
UnhappyAlex <alexandr.bro03@gmail.com>
2026-01-20 23:08:10 +0500

    Changed component CreateFeatureFlagButton

--- src/features/createFeatureFlag/CreateFeatureFlagButton.tsx
+++ src/features/createFeatureFlag/CreateFeatureFlagButton.tsx
@@ -2,14 +2,18 @@ import { useAuth } from "../../providers/UserRoleContext";
 import { useUI } from "../../providers/UIContext";
 import { canCreateFlag } from "../../entities/featureFlag/model/rules";
 
-export default function CreateFeatureFlagButton () {
+export function CreateFeatureFlagButton () {
     const { userRole } = useAuth();
     const { openCreateModal } = useUI();
+    const canCreate = canCreateFlag(userRole);
 
     return (
-        <button className={!canCreateFlag(userRole) ? "opacity-40 cursor-not-allowed" : "hover:opacity-80"} 
-                onClick={openCreateModal}>
-                    + Create Feature Flag
+        <button
+            type="button"
+            disabled={!canCreate}
+            className={`px-1 ${!canCreate ? "opacity-40 cursor-not-allowed" : "hover:opacity-80"}`} 
+            onClick={() => openCreateModal()}>
+                + Create Feature Flag
         </button>
     );
 };
