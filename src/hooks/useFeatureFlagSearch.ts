2b521bdb1ccac8a6f31fd590e229128809284920
UnhappyAlex <alexandr.bro03@gmail.com>
2026-01-22 23:52:45 +0500

    Changed hook useFeatureFlagSearch

--- src/hooks/useFeatureFlagSearch.ts
+++ src/hooks/useFeatureFlagSearch.ts
@@ -1,16 +1,30 @@
 import { useState } from "react";
 import { useFeatureFlagsContext } from "../providers/FeatureFlagsContext";
 import { searchFeatureFlags } from "../features/search/searchFeatureFlags";
+import { useDebouncedValue } from "./useDebouncedValue";
+import { useMemo } from "react";
 
 export function useFeatureFlagSearch () {
     const featureFlags = useFeatureFlagsContext();
     const [search, setSearch] = useState("");
 
-    const filteredFlags = searchFeatureFlags(featureFlags.flags, search);
+    const debouncedSearch = useDebouncedValue(search, 500);
 
+    const filteredFlags = searchFeatureFlags(featureFlags.flags, debouncedSearch);
+
+    const filterSearchState = useMemo(() => {
+        
+    if(!debouncedSearch) {
+        return featureFlags.flags;
+    }
+    
+    return filteredFlags;
+
+    }, [debouncedSearch, filteredFlags]);
+      
     return {
         search,
         setSearch,
-        filteredFlags
-    }
-}
+        filterSearchState
+    };
+};
